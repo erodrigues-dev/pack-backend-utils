551f9f141c4fe3da1328502f3c6534f769139b23
Elton Rodrigues <elton.rodrigues@rededor.com.br>
2024-02-05 15:51:02 -0300

    feat: add authorization parser middleware

--- middlewares/index.js
+++ middlewares/index.js
@@ -2,6 +2,7 @@ import { errorHandler } from './errorHandler.js'
 import { logRequest } from './logRequest.js'
 import { logResponse } from './logResponse.js'
 import { setApplicationVersion } from './setAplicationVersion.js'
+import { setAuthorization } from './setAuthrorization.js'
 import { setContentBody } from './setContentBody.js'
 import { setLogger } from './setLogger.js'
 import { setRequestId } from './setRequestId.js'
@@ -9,6 +10,7 @@ import { setRequestId } from './setRequestId.js'
 export const useBeforeRoutes = (app, config, logger) => {
   setLogger(app, logger)
   setRequestId(app)
+  setAuthorization(app)
   setApplicationVersion(app, config)
   setContentBody(app)
   logRequest(app, config)
