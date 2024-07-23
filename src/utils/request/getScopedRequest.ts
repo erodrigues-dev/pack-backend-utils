c51740d96ea9c3f04f90a5dbb66174999211ced8
Danilo Domingos <danilo.domingos@warren.com.br>
2024-07-23 10:18:33 -0300

    fix: export objects
    Co-authored-by: Danilo Domingos <danilo.domingos@rededor.com.br>

--- src/utils/request/getScopedRequest.ts
+++ src/utils/request/getScopedRequest.ts
@@ -3,7 +3,7 @@ import { getNamespace } from 'cls-hooked'
 export const getScopedRequest = (): { requestId:string, sessionId: string } => {
     const namespace = getNamespace('scoped-request')
     return { 
-        requestId: namespace.get('requestId'),
-        sessionId: namespace.get('sessionId')
+        requestId: namespace?.get('requestId'),
+        sessionId: namespace?.get('sessionId')
     }
 }
